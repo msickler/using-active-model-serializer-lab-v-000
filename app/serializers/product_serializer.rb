c36f4a6ae5c224ad3dc0939e803a4fd0b8b3ec80
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-28 21:49:27 +0000

    automatically backed up by learn

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,4 +1,3 @@
 class ProductSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :inventory, :price
-  has_many :orders, :through => :ordered_products
-end
+  has_many :orders
